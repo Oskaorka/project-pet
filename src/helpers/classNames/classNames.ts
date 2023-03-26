7f294f41eaf7fcc37d9fd1ba0fca0b9b74626f0e
Pavel <63298371+Oskaorka@users.noreply.github.com>
2023-03-26 22:34:57 +0300

    project v.1

---
+++ src/helpers/classNames/classNames.ts
@@ -0,0 +1,23 @@
+type Mods = Record<string, boolean | string>;
+
+// const obj: Mods = {
+//   hovered: 123,
+// };
+
+export function classNames(
+  cls: string,
+  mods: Mods,
+  additional: string[]
+): string {
+  return [
+    cls,
+    ...additional,
+    ...Object.entries(mods)
+      .filter(([className, value]) => Boolean(value))
+      .map(([className]) => className),
+  ].join(" ");
+}
+
+// classNames("remove-btn", { hovered: false, seletable: true, red: false }, [
+//   "pdg",
+// ]);
