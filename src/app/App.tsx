f97d21cba4637eec84fc5d76e956e0b74f62bebe
Pavel <63298371+Oskaorka@users.noreply.github.com>
2023-04-08 16:01:56 +0300

    add jest, and first test

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,15 +1,10 @@
 import "./styles/index.scss";
-
 import { classNames } from "shared/lib/classNames/classNames";
 import { useTheme } from "app/provider/themeProvider";
 import { AppRouter } from "./provider/router";
 import { Navbar } from "widgets/navbar";
 import { Sidebar } from "widgets/Sidebar";
 import { Suspense } from "react";
-// import { useTranslation } from "react-i18next";
-// import { Sidebar } from "widgets/Sidebar";
-
-// const Component = () => {};
 
 const App = () => {
   const { theme } = useTheme();
@@ -18,13 +13,11 @@ const App = () => {
     <div className={classNames("app", {}, [theme])}>
       <Suspense fallback="">
         <Navbar />
-        {/* <Component /> */}
         <div className="content-page">
           <Sidebar />
           <AppRouter />
         </div>
       </Suspense>
-      {/* <button onClick={toggleTheme}>cahange theme</button> */}
     </div>
   );
 };
