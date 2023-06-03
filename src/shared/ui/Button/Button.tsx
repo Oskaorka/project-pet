c7b287f1086d8a5be584f955a1c7558bb7b617ec
Pavel <63298371+Oskaorka@users.noreply.github.com>
2023-06-03 19:38:37 +0300

    Add files via upload

---
+++ src/shared/ui/Button/Button.tsx
@@ -0,0 +1,29 @@
+import { ButtonHTMLAttributes, FC } from 'react';
+import { classNames } from 'shared/lib/classNames/classNames';
+// import { classNames } from '../../lib/classNames/classNames';
+import cls from './button.module.scss';
+
+export enum ThemeButton {
+  CLEAR = 'clear',
+  OUTLINE = 'outline',
+}
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  className?: string;
+  theme?: ThemeButton;
+}
+
+export const Button: FC<ButtonProps> = (props) => {
+    const {
+        className, children, theme, ...otherProps
+    } = props;
+    return (
+        <button
+            type="button"
+            className={classNames(cls.button, {}, [className, cls[theme]])}
+            {...otherProps}
+        >
+            {children}
+        </button>
+    );
+};
