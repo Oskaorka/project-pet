0de70d4ef056695c87a4cb03729c45b6a72ef9ec
Pavel <63298371+Oskaorka@users.noreply.github.com>
2023-06-07 22:32:08 +0300

    add reg-cli,json&html v.0.1

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import { render } from 'react-dom';
 import { ThemeProvider } from 'app/provider/ThemeProvider';
 import { BrowserRouter } from 'react-router-dom';
 import { ErrorBoundary } from 'app/provider/ErrorBondary';
-// import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
 import App from './app/App';
 
 import 'shared/config/i18n/i18n';
