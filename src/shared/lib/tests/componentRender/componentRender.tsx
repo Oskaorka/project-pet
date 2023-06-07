42b6f37127d05a3840a53ce09b8f8a6a6456f85b
Pavel <63298371+Oskaorka@users.noreply.github.com>
2023-06-07 22:29:12 +0300

    add reg-cli,json&html v.0.1

--- src/shared/lib/tests/componentRender/componentRender.tsx
+++ src/shared/lib/tests/componentRender/componentRender.tsx
@@ -4,20 +4,20 @@ import { I18nextProvider } from 'react-i18next';
 import { MemoryRouter } from 'react-router-dom';
 import i18nFortests from 'shared/config/i18n/i18nFortests';
 
-
 export interface componentRenderOptions {
     route?: string
 }
 
 export function componentRender(component: ReactNode, options: componentRenderOptions = {}) {
     const {
-        route = '/'
-    } = options
+        route = '/',
+    } = options;
     return render(
         <MemoryRouter initialEntries={[route]}>
             <I18nextProvider i18n={i18nFortests}>
                 {component}
-            </I18nextProvider>,
-        </MemoryRouter>
+            </I18nextProvider>
+            ,
+        </MemoryRouter>,
     );
 }
